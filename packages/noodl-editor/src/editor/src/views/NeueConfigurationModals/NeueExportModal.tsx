d78fa78033ee6c10fb3c55561e48528b542cccad
Axel Wretman <axel.wretman@neue-labs.com>
2025-04-15 14:39:17 +0200

    Rearranged the USB habndling to better manage multiple runtimes.

--- packages/noodl-editor/src/editor/src/views/NeueConfigurationModals/NeueExportModal.tsx
+++ packages/noodl-editor/src/editor/src/views/NeueConfigurationModals/NeueExportModal.tsx
@@ -2,22 +2,17 @@ import { PrimaryButton } from '@noodl-core-ui/components/inputs/PrimaryButton';
 import { Select } from '@noodl-core-ui/components/inputs/Select';
 import { BaseDialog } from '@noodl-core-ui/components/layout/BaseDialog';
 import { NeueService } from '@noodl-models/NeueServices/NeueService';
-import { SerialPort } from 'electron/main';
 import React, { useEffect, useState } from 'react';
 
 type ModalProps = {
+    title: string,
     isVisible: boolean,
     onClose: () => void,
     jsonData: any,
     devices: any[],
     firmware: string,
-    log: any[],
-    setLog: (log: any[]) => void,
-    writer: WritableStreamDefaultWriter,
-    reader: ReadableStreamDefaultReader,
-    port: SerialPort,
-    setSerial: (serial: any) => void,
-    readStreamer: (reader: any, log: any) => void
+    commands: any[],
+    setCommands: (cmds: any[]) => void,
 };
 
 export default function NeueExportModal(props: ModalProps) {
@@ -32,10 +27,28 @@ export default function NeueExportModal(props: ModalProps) {
             setSetSelectedDevice(null);
             setError(null);
         }
-        console.log(props.reader, props.writer)
     }, [props]);
 
-    async function writeCommand(group, reader) {
+    async function writerCommands (cmds, port, writer) {
+        try {
+            if (selectedDevice === 'USB') {
+                const commands = (await cmds.json()).flat()
+                console.log("commands", commands)
+                for (const group of commands) {
+                    await writeCommand(group, writer)
+                }
+            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            if (selectedDevice === 'USB') {
+                if (writer) writer.releaseLock()
+                if (port) port.close()
+            }
+        }
+}
+
+    async function writeCommand(group, writer) {
         new Promise((resolve) => {
             // Assuming `cmd` is an array of hex strings like ["0x00", "0x01", "0x02", ...] \xff\x01\x00\x00
             // const cmd = ["0xff", "0x01", "0x00", "0x00"];  // Example cmd array
@@ -57,22 +70,39 @@ export default function NeueExportModal(props: ModalProps) {
             // Step 5: Send the data via port (assuming `port` is defined and open)
             // port.write(combined);
             console.log("writing message", combined)
-            props.writer.write(combined).then(() => {
+            writer.write(combined).then(() => {
                 setTimeout(() => {
                     console.log("message written")
                     resolve(true)
                 }, 1000)
             });
-        // const id = setTimeout(() => {
-        //     console.log("Message response timeout")
-        //     reader.cancel()
-        //     writeCommand(group, writer, reader)
-        // }, 5000);
-        // const { done, value } = await reader.read()
-        // console.log("Message response: ", value, done)
-        // props.setLog([...props.log, value])
-        // clearTimeout(id)
         })
+    }
+
+    async function onClick() {
+        setIsLoading(true);
+        let commands: Response | string[] = []
+        if (props.commands.length > 0) {
+            commands = props.commands
+            props.setCommands([])
+        } else {
+            console.log(props.jsonData)
+            commands = await NeueService.instance.pushFlow(selectedDevice, props.jsonData, props.firmware);
+        }
+        if (selectedDevice !== 'USB') return
+        // @ts-ignore
+        const p = await navigator.serial.requestPort()
+
+        try {
+            await p.open({baudRate: 115200, bufferSize: 255});
+        }
+        catch (error) {
+            console.log("Error opening port: ", error)
+        }
+        const w = p.writable.getWriter()
+        // r = p.readable.getReader()
+        writerCommands(commands, p, w)
+        setIsLoading(false);
     }
 
     return (
@@ -80,6 +110,7 @@ export default function NeueExportModal(props: ModalProps) {
             isVisible={props.isVisible}
             onClose={props.onClose}
             isLockingScroll
+            title={props.title}
         >
             <div style={{ width: 400 }}>
                 <div
@@ -106,48 +137,7 @@ export default function NeueExportModal(props: ModalProps) {
 
                     {error && <div style={{ color: 'red' }}>{error}</div>}
 
-                    <PrimaryButton label="Push to device" onClick={async () => {
-                        setIsLoading(true);
-                        try {
-                            const response = await NeueService.instance.pushFlow(selectedDevice, props.jsonData, props.firmware);
-                            const commands = (await response.json()).flat()
-                            console.log("commands", commands)
-
-                            // props.reader.cancel()
-                            // await props.reader.closed
-                            // @ts-ignore
-                            // const reader = props.port.readable.getReader()'
-                            // await writeCommand(commands[0], props.reader)
-                            // console.log("Message response: ", await props.reader.read())
-                            // return
-                            // console.log("Message response2: ", value, done)
-                            if (selectedDevice === 'USB') {
-                                for (const group of commands) {
-                                    await writeCommand(group, props.reader)
-                                }
-                                // props.reader.read()
-                                // @ts-ignore
-                                // props.setSerial({port: props.port, reader: props.reader, writer: props.writer})
-                                props.readStreamer(props.reader, [])
-                            }
-                        } catch (error) {
-                            console.log(error)
-                        } finally {
-                            // props.reader.releaseLock()
-                            props.writer.releaseLock()
-                            // @ts-ignore
-                            props.port.close()
-                            setIsLoading(false);
-                        }
-                        // const response = await NeueService.instance.pushFlow(selectedDevice, props.jsonData, props.firmware);
-                        // if (response.status === 200) {
-                        //     props.onClose();
-                        // } else {
-                        //     const body = await response.json();
-                        //     setError('Failed to push to device: ' + JSON.stringify(body));
-                        // }
-                        setIsLoading(false);
-                    }}
+                    <PrimaryButton label="Push to device" onClick={onClick}
                         isLoading={isLoading}
                         isDisabled={isLoading} />
                 </div>
--- packages/noodl-editor/src/editor/src/views/panels/iENBLPanel/iENBLPanel.tsx
+++ packages/noodl-editor/src/editor/src/views/panels/iENBLPanel/iENBLPanel.tsx
@@ -14,9 +14,8 @@ import { NeueService } from '@noodl-models/NeueServices/NeueService';
 import { isComponentModel_NeueRuntime } from '@noodl-utils/NodeGraph';
 import NeueExportModal from '../../NeueConfigurationModals/NeueExportModal';
 import { App } from '@noodl-models/app';
-import { TextInput } from '@noodl-core-ui/components/inputs/TextInput';
-import { TextArea } from '@noodl-core-ui/components/inputs/TextArea';
-import { Text } from '@noodl-core-ui/components/typography/Text';
+import { CollapsableSection } from '@noodl-core-ui/components/sidebar/CollapsableSection';
+import { SectionVariant } from '@noodl-core-ui/components/sidebar/Section';
 
 export function iENBLPanel() {
   const environment = useActiveEnvironment(ProjectModel.instance);
@@ -29,22 +28,15 @@ export function iENBLPanel() {
   const [jsonData, setJsonData] = useState({});
   const [isExportModalOpen, setIsExportModalOpen] = useState(false);
 
-  // const [port, setPort] = useState(null);
-  // const [writer, setWriter] = useState(null);
-  // const [reader, setReader] = useState(null);
-  const [serial, setSerial] = useState({port: null, writer: null, reader: null});
-  const [log, setLog] = useState([]);
+  const [commands, setCommands] = useState([]);
+
+  const [exportModalTitle, setExportModalTitle] = useState("");
 
   useEffect(() => {
     NeueService.instance.load().then((result) => {
       fetchDevices();
     });
   }, [setLoading]);
-  useEffect(() => {
-    // setupStream().then(readStream)
-    console.log("Serial has changed", serial)
-    setupStream()
-  }, [serial]);
   // useMemo(readStream, [serial])
   const componentPanelOptions = {
     showSheetList: false,
@@ -68,142 +60,6 @@ export function iENBLPanel() {
     }).finally(() => {
       setLoading(false);
     });
-  }
-
-  async function setupStream() {
-    console.log(serial)
-    let p = serial.port
-    let r = serial.reader
-    let w = serial.writer
-    if (p == null && "serial" in navigator || p.readable == null || p.writable == null) {
-      // @ts-ignore
-      p = await navigator.serial.requestPort()
-
-      try {
-        await p.open({baudRate: 115200, bufferSize: 255});
-      }
-      catch (error) {
-        console.log("Error opening port: ", error)
-      }
-      w = p.writable.getWriter()
-      // r = p.readable.getReader()
-      console.log("Setting writer", {p, r, w})
-      setSerial({port: p, reader: r, writer: w})
-    } else {
-      if (r == null) {
-        // r = p.readable.getReader()
-        // setSerial({port: p, reader: r, writer: w})
-      }
-      if (w == null) {
-        w = p.writable.getWriter()
-        setSerial({port: p, reader: r, writer: w})
-      }
-    }
-    // console.log("reading", await r.read())
-    // console.log("reading")
-    // const {value, done} = await r.read()
-    // console.log("Read value: ", value)
-    // const temp = [...log, Array.from(value)]
-    // console.log(log)
-    // setLog(temp)
-    // document.dispatchEvent(new CustomEvent('log', {detail: Array.from(value)}))
-    // if (!done) {
-    // }
-  }
-
-  async function resetStream() {
-    const p = serial.port
-    const r = serial.reader
-    const w = serial.writer
-    console.log("Resetting stream...")
-    if (p != null && "serial" in navigator) {
-      try {
-        if (r != null) {
-          await r.releaseLock()
-        }
-      } catch (error) {
-        console.log("Error releasing reader lock: ", error)
-      }
-      try {
-        if (w != null) {
-          await w.releaseLock()
-        }
-      } catch (error) {
-        console.log("Error releasing writer lock: ", error)
-      }
-
-      console.log("Closing port...")
-      await p.close()
-      serial.port = null
-      serial.writer = null
-      console.log(p)
-      setSerial({port: null, reader: null, writer: null})
-    }
-  }
-
-  async function readStream() {
-    console.log("Read stream...", serial.reader)
-    if (serial.reader != null) {
-      try {
-        const {value, done} = await serial.reader.read()
-        console.log("Read value: ", value)
-        const temp = [...log, Array.from(value)]
-        console.log(temp)
-        setLog(temp)
-        if (!done) {
-          readStream()
-        }
-      } catch (error) {
-        console.log("Error reading stream: ", error)
-        setSerial({...serial, reader: null})
-      }
-    }
-  }
-  async function readStreamer(reader, message = [], log = []) {
-    // console.log("Read stream...", serial.reader)
-    return
-    if (reader != null) {
-      try {
-        const {value, done} = await reader.read()
-        if (value == undefined) {
-          console.log("Undefined values in read stream", value, done)
-          readStreamer(reader, [], log)
-          return
-        }
-        let values = value
-        console.log("Read value: ", values)
-        if (message.length < 4) {
-          message = [...message, ...Array.from(values)]
-          console.log("Message1: ", message)
-          readStreamer(reader, message, log)
-          return
-        } else {
-          if (message[0] == 0xAA && message[1] == 0xBB) {
-            const length = message[3]
-            if (message.length < length + 4) {
-              message = [...message, ...Array.from(values)]
-              console.log("Message2: ", message)
-              readStreamer(reader, message, log)
-              return
-            } else if (message.length > length + 4) {
-              message = message.slice(0, length + 4)
-              values = values.slice(length + 4)
-            }
-          }
-        }
-        console.log("Complete message: ", message)
-        log = [...log, message]
-        console.log(log)
-        setLog(log)
-        if (!done) {
-          readStreamer(reader,Array.from(value), log)
-        }
-      } catch (error) {
-        console.log("Error reading stream: ", error)
-        setupStream()
-        // setSerial({...serial, reader: null})
-      }
-    }
   }
 
   function handleCloseModal() {
@@ -265,8 +121,6 @@ export function iENBLPanel() {
 
     if (neueRoot) {
       setLoading(true);
-      // await resetStream()
-      // setupStream()
       const rootNodes = neueRoot?.graph?.getNodeSetWithNodes(neueRoot.getNodes());
       const expandedNodes = findAndExpandNodes(rootNodes.nodes, allComponents);
 
@@ -274,13 +128,19 @@ export function iENBLPanel() {
         ...neueRoot.toJSON(),
         nodes: expandedNodes
       };
+      console.log(root)
       setJsonData(root);
     }
     setLoading(false);
-
+    setExportModalTitle("Push Flow to Device")
     setIsExportModalOpen(!isExportModalOpen);
   }
 
+  async function sendRestartCommand() {
+    setExportModalTitle("Restart device")
+    setCommands([{cmd: '07 90 00 01 00', index: 0, comment: 'Restart device'}])
+    setIsExportModalOpen(!isExportModalOpen);
+  }
 
   return (
     <BasePanel title="Neue Playground" isFill>
@@ -289,15 +149,12 @@ export function iENBLPanel() {
           <VStack>
             <PrimaryButton label="Push Flow to Device" onClick={getJsonConfiguration} isDisabled={loading} />
           </VStack>
-          <Box hasXSpacing hasYSpacing>
-            <VStack>
-              <Text>{`USB Device: ${serial.port != null && serial.reader != null && serial.writer != null ? "Connected" : "Disconnected"}`}</Text>
-            </VStack>
-            <VStack>
-              <TextArea label="USB Debug" value={log.reduce((accumulator, currentValue) => {return accumulator += "\n" + currentValue}, "")} isDisabled={true}/>
-            </VStack>
-          </Box>
         </Box>
+        <CollapsableSection title="Device Commands" variant={SectionVariant.Panel} hasTopDivider hasBottomSpacing hasGutter isClosed={true}>
+            <VStack>
+              <PrimaryButton label="Restart" onClick={sendRestartCommand} isDisabled={loading} />
+            </VStack>
+        </CollapsableSection>
         <div style={{ flex: '1', overflow: 'hidden' }}>
           <ComponentsPanel options={componentPanelOptions} />
         </div>
@@ -308,7 +165,7 @@ export function iENBLPanel() {
           </VStack>
         </Box>
       </Container>
-      <NeueExportModal readStreamer={readStreamer} log={log} setLog={setLog} setSerial={setSerial} port={serial.port} writer={serial.writer} reader={serial.reader} onClose={handleCloseModal} isVisible={isExportModalOpen} jsonData={jsonData} devices={devices} firmware={ProjectModel.instance.firmware} />
+      <NeueExportModal title={exportModalTitle} commands={commands} setCommands={setCommands} onClose={handleCloseModal} isVisible={isExportModalOpen} jsonData={jsonData} devices={devices} firmware={ProjectModel.instance.firmware} />
 
     </BasePanel>
 
